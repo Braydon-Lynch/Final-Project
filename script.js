1e8d401dce71ee93ae0cc0f8c4dc466d1789b373
Braydon Lynch <bgl1999@hotmail.com>
2023-04-05 09:54:35 -0300

    started making area in or population

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+
+//read data from csv file
 function getCountryInfo(menuRef)
 {
-    //declare a ref to the form
+    //declare a reference to the form
     let formRef = document.querySelector("#countryInfoForm");
     let selectedCountry = formRef["countrySelection"].value; 
+    let pUnitSelected = formRef["pUnitSelection"].value;
     alert(selectedCountry)
 
     //change the href or the wikipage link/button
     let wikiLink = document.querySelector("#wikiPageLink");
     wikiLink.setAttribute("href", "https://en.wikipedia.org/wiki/" + selectedCountry);
 }
+
+//calculate populaton %
+
+//calculate population density
+
+//convert sq miles to sq km and vice versa
+
+//convert per sq miles to per sq km and vice versa
