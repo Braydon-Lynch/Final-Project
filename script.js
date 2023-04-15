5bbab05fd78cc1317e2e88cf5124cbbc9eb30670
Braydon Lynch <bgl1999@hotmail.com>
2023-04-15 18:04:13 -0300

    created population, working

--- script.js
+++ script.js
@@ -31,51 +31,29 @@ function getCountryInfo(menuRef)
     let selectedCountry = formRef["countrySelection"].value; 
     let pUnitSelected = formRef["pUnitSelection"].value;
 
+    //display population of the selected country
+    let populationDiv = document.querySelector("#population");
+    for(let i=0; i<countryInfo.length; i++){
+        if(countryInfo[i].Name===selectedCountry){
+            populationDiv.innerHTML=""+countryInfo[i].Population;
+            break;
+        }
+    }
 
     //inserting image
     let flagImgDiv = document.querySelector("#flagImg");
     let imgHTML = `<img src="flags/${selectedCountry}.png"></img>`;
     flagImgDiv.innerHTML = imgHTML;
 
+
     //change the href or the wikipage link/button
     let wikiLink = document.querySelector("#wikiPageLink");
-    wikiLink.setAttribute("href", "https://en.wikipedia.org/wiki/" + selectedCountry);
+    wikiLink.setAttribute("href", "https://en.wikipedia.org/wiki/" + selectedCountry); 
 }
 
-function calcPopulation()
-{
-    
-}
 
 //calculate population density
 
 //convert sq miles to sq km and vice versa
 
 //convert per sq miles to per sq km and vice versa
-
-
-
-/*
-//get the users country selection
-let countrySelection = document.getElementById("countrySelection");
-
-//get population data from the .json file
-fetch("countries.json")
-    .then(response=>response.json())
-    .then(data=>{
-        const population = data[selectedCountry];
-
-        //population is available or not
-        if(population){
-            document.getElementById("result").innerHTML= `The population of ${selectedCountry} is ${population}`;
-        }else{
-            document.getElementById("result").innerHTML= `Population data not available for selected country`;
-        }
-        return;
-    })
-    .catch(error=>{
-        console.error(error);
-        document.getElementById("result").innerHTML= `An error has occured`;
-        return;
-    });
-    */
